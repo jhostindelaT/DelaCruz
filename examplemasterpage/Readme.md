81899ac46e0b71d86e2fa6330b631479273613ec
mr.teclado <jhostin.cruz12346@gmail.com>
2021-10-24 18:20:47 -0600

    Primera prueba de MasteraPage

---
+++ examplemasterpage/Readme.md
@@ -0,0 +1,3 @@
+# Cre una primer prueba de masterpage con masterpage
+
+ ### Bueno, desidi hacerlo de esta manera para no complicarme mucho por falta de intener, esta marterpage estara temporlmenete hasta que encuentre manera mas interesante de hacerlo sin laravel. Tambien para el futuro pienso conectarlo con la base de datos.
---
+++ examplemasterpage/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hola</title>
+</head>
+<body>
+    
+    <a href="index.php"> home</a>
+    <a href="ayuda.php"> ayuda</a>
+    <a href="cosas.php"> cosas</a>
+
+</body>
+</html>
