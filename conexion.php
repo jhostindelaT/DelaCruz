bb5627c091d8029c4223e6d757a6562daf975cea
Jhostin <jhostin.cruz12346@gmail.com>
2021-11-14 17:36:49 -0600

    Regreso

--- conexion.php
+++ conexion.php
@@ -1,15 +1,15 @@
 <?php
 
 
-    $user="jhostinsi";
+    $user="JhostinDelaC";
     $pass="Jhostin2004@";
     $server="localhost";
-    $db="Usuarios";
+    $db="Clientes";
     $con= new mysqli($server,$user,$pass,$db);
 
     if($con-> connect_errno){
 
-        die('la conexxion ha fallado'. $con-> connect_errno);
+        die('la conexion ha fallado'. $con-> connect_errno);
 
     }
     else{
